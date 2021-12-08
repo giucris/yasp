73ca5147630565e9bce11f675d0c4e5459f3946b
Giuseppe Cristiano <giucristiano89@gmail.com>
2021-12-08 18:25:32 +0100

    Fixed missing file on previous commit

---
+++ yasp-core/src/main/scala/it/yasp/core/spark/model/Csv.scala
@@ -0,0 +1,3 @@
+package it.yasp.core.spark.model
+
+case class Csv(path: String, header: Boolean, separator: String)
---
+++ yasp-core/src/main/scala/it/yasp/core/spark/model/Parquet.scala
@@ -0,0 +1,3 @@
+package it.yasp.core.spark.model
+
+case class Parquet(path: String, mergeSchema: Boolean)
