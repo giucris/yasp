18eb8a30009bbcb77b3b6410ab2ef349d3667f04
giucris <giucristiano89@gmail.com>
2022-03-17 21:46:17 +0100

    Added table registry (#11)

---
+++ yasp-core/src/test/scala/it/yasp/core/spark/registry/RegistryTest.scala
@@ -0,0 +1,38 @@
+package it.yasp.core.spark.registry
+
+import it.yasp.core.spark.registry.Registry.DefaultRegistry
+import it.yasp.testkit.SparkTestSuite
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.DataTypes.StringType
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.{BeforeAndAfterAll, DoNotDiscover}
+
+@DoNotDiscover
+class RegistryTest extends AnyFunSuite with SparkTestSuite with BeforeAndAfterAll {
+
+  override protected def beforeAll(): Unit =
+    super.beforeAll()
+
+  override protected def afterAll(): Unit =
+    super.afterAll()
+
+  test("register") {
+    val expected = spark.createDataset(Seq(Row("h1", "h2", "h3"), Row("a", "b", "c")))(
+      RowEncoder(
+        StructType(
+          Seq(
+            StructField("_c0", StringType, nullable = true),
+            StructField("_c1", StringType, nullable = true),
+            StructField("_c2", StringType, nullable = true)
+          )
+        )
+      )
+    )
+    new DefaultRegistry().register(expected, "test_table")
+    val actual   = spark.table("test_table")
+    assertDatasetEquals(actual, expected)
+  }
+
+}
