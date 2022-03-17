e3108c554d4ae7ba5a81b10abd66f7e8a4e438d6
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-03-17 21:42:42 +0100

    Added registry test

--- yasp-core/src/test/scala/it/yasp/core/spark/YaspCoreSuites.scala
+++ yasp-core/src/test/scala/it/yasp/core/spark/YaspCoreSuites.scala
@@ -2,6 +2,7 @@ package it.yasp.core.spark
 
 import it.yasp.core.spark.processor.SqlProcessorTest
 import it.yasp.core.spark.reader._
+import it.yasp.core.spark.registry.RegistryTest
 import it.yasp.core.spark.session.SparkSessionFactoryTest
 import it.yasp.core.spark.writer.ParquetWriterTest
 import org.scalatest.Stepwise
@@ -16,5 +17,6 @@ class YaspCoreSuites
       new AvroReaderTest,
       new XmlReaderTest,
       new SqlProcessorTest,
-      new ParquetWriterTest
+      new ParquetWriterTest,
+      new RegistryTest
     )
