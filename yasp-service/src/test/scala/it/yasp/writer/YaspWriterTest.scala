169f299ad8e7293decc940e61a1bf1aff6893055
giucris <giucristiano89@gmail.com>
2022-03-19 10:10:14 +0100

    Added YaspSink YaspWriter and DefaultImpl (#17)

---
+++ yasp-service/src/test/scala/it/yasp/writer/YaspWriterTest.scala
@@ -0,0 +1,39 @@
+package it.yasp.writer
+
+import it.yasp.core.spark.model.Dest
+import it.yasp.core.spark.model.Dest._
+import it.yasp.core.spark.registry.Registry
+import it.yasp.core.spark.writer.Writer
+import it.yasp.model.YaspSink
+import it.yasp.testkit.SparkTestSuite
+import it.yasp.writer.YaspWriter.DefaultYaspWriter
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.DataTypes.StringType
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.funsuite.AnyFunSuite
+
+class YaspWriterTest extends AnyFunSuite with SparkTestSuite with MockFactory {
+
+  test("write") {
+    val registry = mock[Registry]
+    val writer   = mock[Writer[Dest]]
+
+    inSequence(
+      (registry.retrieve _)
+        .expects("id")
+        .once()
+        .returns(
+          spark.createDataset(Seq(Row("a")))(
+            RowEncoder(StructType(Seq(StructField("h1", StringType, nullable = true))))
+          )
+        ),
+      (writer.write _)
+        .expects(*, Parquet("path"))
+        .once()
+    )
+    new DefaultYaspWriter(registry, writer).write(YaspSink("id", Parquet("path")))
+  }
+
+}
