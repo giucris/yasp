4a1cbb0e18d8c018a162926729d866cafbe9c651
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-03-19 10:44:03 +0100

    Fix format

--- yasp-service/src/test/scala/it/yasp/service/YaspServiceTest.scala
+++ yasp-service/src/test/scala/it/yasp/service/YaspServiceTest.scala
@@ -12,86 +12,85 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class YaspServiceTest extends AnyFunSuite with MockFactory {
 
-  val loader: YaspLoader = mock[YaspLoader]
+  val loader: YaspLoader       = mock[YaspLoader]
   val processor: YaspProcessor = mock[YaspProcessor]
-  val writer: YaspWriter = mock[YaspWriter]
+  val writer: YaspWriter       = mock[YaspWriter]
 
-  val yaspService = new DefaultYaspService(loader,processor,writer)
+  val yaspService = new DefaultYaspService(loader, processor, writer)
 
   test("exec with 1 source and 1 sink") {
     inSequence(
       (loader.load _)
-        .expects(YaspSource("id1",Source.Json(Seq("sourcePath"))))
+        .expects(YaspSource("id1", Source.Json(Seq("sourcePath"))))
         .once(),
       (writer.write _)
-        .expects(YaspSink("id1",Dest.Parquet("destPath")))
+        .expects(YaspSink("id1", Dest.Parquet("destPath")))
         .once()
     )
 
     yaspService.exec(
-      sources=Seq(YaspSource("id1",Source.Json(Seq("sourcePath")))),
-      processes=Seq.empty,
-      sinks=Seq(YaspSink("id1",Dest.Parquet("destPath")))
+      sources = Seq(YaspSource("id1", Source.Json(Seq("sourcePath")))),
+      processes = Seq.empty,
+      sinks = Seq(YaspSink("id1", Dest.Parquet("destPath")))
     )
   }
 
   test("exec with 1 source 1 process 1 sink") {
     inSequence(
       (loader.load _)
-        .expects(YaspSource("id1",Source.Json(Seq("sourcePath"))))
+        .expects(YaspSource("id1", Source.Json(Seq("sourcePath"))))
         .once(),
       (processor.process _)
-        .expects(YaspProcess("id2",Sql("my-sql")))
+        .expects(YaspProcess("id2", Sql("my-sql")))
         .once(),
       (writer.write _)
-        .expects(YaspSink("id2",Dest.Parquet("destPath")))
+        .expects(YaspSink("id2", Dest.Parquet("destPath")))
         .once()
     )
 
     yaspService.exec(
-      sources=Seq(YaspSource("id1",Source.Json(Seq("sourcePath")))),
-      processes=Seq(YaspProcess("id2",Sql("my-sql"))),
-      sinks=Seq(YaspSink("id2",Dest.Parquet("destPath")))
+      sources = Seq(YaspSource("id1", Source.Json(Seq("sourcePath")))),
+      processes = Seq(YaspProcess("id2", Sql("my-sql"))),
+      sinks = Seq(YaspSink("id2", Dest.Parquet("destPath")))
     )
   }
 
   test("exec with n source n process n sink") {
     inSequence(
       (loader.load _)
-        .expects(YaspSource("id1",Source.Json(Seq("sourcePath1"))))
+        .expects(YaspSource("id1", Source.Json(Seq("sourcePath1"))))
         .once(),
       (loader.load _)
-        .expects(YaspSource("id2",Source.Parquet(Seq("sourcePath2"),mergeSchema = true)))
+        .expects(YaspSource("id2", Source.Parquet(Seq("sourcePath2"), mergeSchema = true)))
         .once(),
       (processor.process _)
-        .expects(YaspProcess("id3",Sql("my-sql-1")))
+        .expects(YaspProcess("id3", Sql("my-sql-1")))
         .once(),
       (processor.process _)
-        .expects(YaspProcess("id4",Sql("my-sql-2")))
+        .expects(YaspProcess("id4", Sql("my-sql-2")))
         .once(),
       (writer.write _)
-        .expects(YaspSink("id4",Dest.Parquet("destPath1")))
+        .expects(YaspSink("id4", Dest.Parquet("destPath1")))
         .once(),
       (writer.write _)
-        .expects(YaspSink("id3",Dest.Parquet("destPath2")))
+        .expects(YaspSink("id3", Dest.Parquet("destPath2")))
         .once()
     )
 
     yaspService.exec(
-      sources=Seq(
-        YaspSource("id1",Source.Json(Seq("sourcePath1"))),
-        YaspSource("id2",Source.Parquet(Seq("sourcePath2"),mergeSchema = true)),
+      sources = Seq(
+        YaspSource("id1", Source.Json(Seq("sourcePath1"))),
+        YaspSource("id2", Source.Parquet(Seq("sourcePath2"), mergeSchema = true))
       ),
-      processes=Seq(
-        YaspProcess("id3",Sql("my-sql-1")),
-        YaspProcess("id4",Sql("my-sql-2"))
+      processes = Seq(
+        YaspProcess("id3", Sql("my-sql-1")),
+        YaspProcess("id4", Sql("my-sql-2"))
       ),
-      sinks=Seq(
-        YaspSink("id4",Dest.Parquet("destPath1")),
-        YaspSink("id3",Dest.Parquet("destPath2"))
+      sinks = Seq(
+        YaspSink("id4", Dest.Parquet("destPath1")),
+        YaspSink("id3", Dest.Parquet("destPath2"))
       )
     )
   }
 
 }
-
