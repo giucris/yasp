ec220b254268af477ad74c7537d197ea7046695a
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-03-21 11:38:01 +0100

    Added YaspExecutor

---
+++ yasp-app/src/main/scala/it/yasp/executor/YaspExecutor.scala
@@ -0,0 +1,25 @@
+package it.yasp.executor
+
+import it.yasp.core.spark.session.SparkSessionFactory
+import it.yasp.model.YaspExecution
+import it.yasp.service.YaspServiceFactory
+
+trait YaspExecutor {
+  def exec(yaspExecution: YaspExecution)
+}
+
+object YaspExecutor {
+
+  class DefaultYaspExecutor(
+      sessionFactory: SparkSessionFactory,
+      yaspServiceFactory: YaspServiceFactory
+  ) extends YaspExecutor {
+
+    override def exec(yaspExecution: YaspExecution): Unit = {
+      val session = sessionFactory.create(yaspExecution.sessionConf)
+      yaspServiceFactory.create(session).run(yaspExecution.yaspPlan)
+    }
+
+  }
+
+}
--- yasp-app/src/test/scala/it/yasp/executor/YaspExecutorTest.scala
+++ yasp-app/src/test/scala/it/yasp/executor/YaspExecutorTest.scala
@@ -1,42 +1,122 @@
 package it.yasp.executor
 
+import it.yasp.core.spark.model.Process.Sql
 import it.yasp.core.spark.model.{Dest, Source}
 import it.yasp.core.spark.session.SessionType.Local
 import it.yasp.core.spark.session.{SessionConf, SparkSessionFactory}
-import it.yasp.model.{YaspExecution, YaspPlan, YaspSink, YaspSource}
+import it.yasp.executor.YaspExecutor.DefaultYaspExecutor
+import it.yasp.model._
 import it.yasp.service.YaspServiceFactory
+import it.yasp.testkit.{SparkTestSuite, TestUtils}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.DataTypes.StringType
+import org.apache.spark.sql.types.{StructField, StructType}
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
-class YaspExecutorTest extends AnyFunSuite with MockFactory {
+class YaspExecutorTest
+    extends AnyFunSuite
+    with MockFactory
+    with BeforeAndAfterAll
+    with SparkTestSuite {
+
+  private val workspace = "yasp-app/src/test/resources/YaspExecutorTest"
+
+  override protected def beforeAll(): Unit = {
+    TestUtils.cleanFolder(workspace)
+    super.beforeAll()
+  }
+
+  override protected def afterAll(): Unit = {
+    TestUtils.cleanFolder(workspace)
+    super.afterAll()
+  }
 
   test("exec") {
-    val sparkSessionFactory = mock[SparkSessionFactory]
-    val yaspServiceFactory  = mock[YaspServiceFactory]
+    TestUtils.createFile(
+      s"$workspace/csv-data-source-1/file1.csv",
+      Seq(
+        "id,name,surname",
+        "1,name-1,surname-1",
+        "2,name-2,surname-2"
+      )
+    )
 
-    new DefaultYaspExecutor(sparkSessionFactory, yaspServiceFactory).exec(
-      YaspExecution(
-        sessionConf = SessionConf(Local, "my-app-name", Map.empty),
-        yaspPlan = YaspPlan(
-          sources = Seq(YaspSource("id", Source.Parquet(Seq("xyz"), mergeSchema = false))),
-          processes = Seq.empty,
-          sinks = Seq(YaspSink("id", Dest.Parquet("my-path")))
+    TestUtils.createFile(
+      s"$workspace/csv-data-source-2/file1.csv",
+      Seq(
+        "id,city,address",
+        "1,city-1,address-1",
+        "2,city-2,address-2"
+      )
+    )
+
+    val sparkSessionFactory = mock[SparkSessionFactory]
+
+    (sparkSessionFactory.create _)
+      .expects(SessionConf(Local, "my-app-name", Map.empty))
+      .once()
+      .returns(spark)
+
+    new DefaultYaspExecutor(sparkSessionFactory, new YaspServiceFactory())
+      .exec(
+        YaspExecution(
+          sessionConf = SessionConf(Local, "my-app-name", Map.empty),
+          yaspPlan = YaspPlan(
+            sources = Seq(
+              YaspSource(
+                "data_1",
+                Source.Csv(
+                  paths = Seq(s"$workspace/csv-data-source-1/file1.csv"),
+                  header = true,
+                  separator = ","
+                )
+              ),
+              YaspSource(
+                "data_2",
+                Source.Csv(
+                  paths = Seq(s"$workspace/csv-data-source-2/file1.csv"),
+                  header = true,
+                  separator = ","
+                )
+              )
+            ),
+            processes = Seq(
+              YaspProcess(
+                "data_3",
+                Sql("SELECT d1.*,d2.city,d2.address FROM data_1 d1 JOIN data_2 d2 ON d1.id=d2.id")
+              )
+            ),
+            sinks = Seq(
+              YaspSink("data_3", Dest.Parquet(s"$workspace/parquet-out/"))
+            )
+          )
+        )
+      )
+
+    val actual   = spark.read.parquet(s"$workspace/parquet-out/")
+    val expected = spark.createDataset(
+      Seq(
+        Row("1", "name-1", "surname-1", "city-1", "address-1"),
+        Row("2", "name-2", "surname-2", "city-2", "address-2")
+      )
+    )(
+      RowEncoder(
+        StructType(
+          Seq(
+            StructField("id", StringType, nullable = true),
+            StructField("name", StringType, nullable = true),
+            StructField("surname", StringType, nullable = true),
+            StructField("city", StringType, nullable = true),
+            StructField("address", StringType, nullable = true)
+          )
         )
       )
     )
+
+    assertDatasetEquals(actual,expected)
   }
 
-  trait YaspExecutor {
-    def exec(yaspExecution: YaspExecution)
-  }
-
-  class DefaultYaspExecutor(
-      sessionFactory: SparkSessionFactory,
-      yaspServiceFactory: YaspServiceFactory
-  ) extends YaspExecutor {
-    override def exec(yaspExecution: YaspExecution): Unit = {
-      val session = sessionFactory.create(yaspExecution.sessionConf)
-      yaspServiceFactory.create(session).run(yaspExecution.yaspPlan)
-    }
-  }
 }
