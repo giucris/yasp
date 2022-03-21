93117a62ac209de36853b69d8602d4371199d944
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-03-21 11:49:46 +0100

    Added YaspServiceFactory

---
+++ yasp-service/src/test/scala/it/yasp/service/YaspServiceFactoryTest.scala
@@ -0,0 +1,14 @@
+package it.yasp.service
+
+import it.yasp.service.YaspService.DefaultYaspService
+import it.yasp.testkit.SparkTestSuite
+import org.scalatest.funsuite.AnyFunSuite
+
+class YaspServiceFactoryTest extends AnyFunSuite with SparkTestSuite {
+
+  test("create"){
+    val actual = new YaspServiceFactory().create(spark)
+    assert(actual.isInstanceOf[DefaultYaspService])
+  }
+
+}
