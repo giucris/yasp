40f71e75b3474692682d3b3c04fe0c29547f533f
giucris <giucristiano89@gmail.com>
2022-03-21 10:50:16 +0100

    Added ProcessProcessor, DestWriter and YaspPlan model to YaspService (#21)

---
+++ yasp-core/src/test/scala/it/yasp/core/spark/writer/DestWriterTest.scala
@@ -0,0 +1,44 @@
+package it.yasp.core.spark.writer
+
+import it.yasp.core.spark.model.Dest.Parquet
+import it.yasp.core.spark.writer.Writer.{DestWriter, ParquetWriter}
+import it.yasp.testkit.{SparkTestSuite, TestUtils}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.scalatest.{BeforeAndAfterAll, DoNotDiscover}
+import org.scalatest.funsuite.AnyFunSuite
+
+@DoNotDiscover
+class DestWriterTest extends AnyFunSuite with SparkTestSuite with BeforeAndAfterAll {
+  private val workspace = "yasp-core/src/test/resources/DestWriterTest"
+
+  override protected def beforeAll(): Unit = {
+    TestUtils.cleanFolder(workspace)
+    super.beforeAll()
+  }
+
+  override protected def afterAll(): Unit = {
+    TestUtils.cleanFolder(workspace)
+    super.afterAll()
+  }
+
+  test("write dest parquet") {
+    val expected = spark.createDataset(Seq(Row("a", "b", "c")))(
+      RowEncoder(
+        StructType(
+          Seq(
+            StructField("h0", StringType, nullable = true),
+            StructField("h1", StringType, nullable = true),
+            StructField("h2", StringType, nullable = true)
+          )
+        )
+      )
+    )
+    new DestWriter().write(expected, Parquet(s"$workspace/parquet1/"))
+    val actual   = spark.read.parquet(s"$workspace/parquet1/")
+
+    assertDatasetEquals(actual, expected)
+  }
+
+}
