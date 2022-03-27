3a75f9bcc71ad0e35c1fab261eca05107e0b6c30
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-03-27 22:14:55 +0200

    Added Cache DefaultCache and CacheLayer

--- yasp-service/src/main/scala/it/yasp/service/processor/YaspProcessor.scala
+++ yasp-service/src/main/scala/it/yasp/service/processor/YaspProcessor.scala
@@ -11,20 +11,24 @@ import it.yasp.service.model.YaspProcess
   * Provide a unified way to execute a [[YaspProcess]]
   */
 trait YaspProcessor {
-  /**
-    * Execute the provided [[YaspProcess]] cache the result if some cache option are specified on the process and register the result as a table.
-    * @param process: A [[YaspProcess]] instance
+
+  /** Execute the provided [[YaspProcess]] cache the result if some cache option are specified on
+    * the process and register the result as a table.
+    * @param process:
+    *   A [[YaspProcess]] instance
     */
   def process(process: YaspProcess)
 }
 
 object YaspProcessor {
 
-  /**
-    * DefaultYaspProcessor implementation
-    * @param processor: A [[Processor]] instance
-    * @param registry: A [[Registry]] instance
-    * @param cache: A [[Cache]] instance
+  /** DefaultYaspProcessor implementation
+    * @param processor:
+    *   A [[Processor]] instance
+    * @param registry:
+    *   A [[Registry]] instance
+    * @param cache:
+    *   A [[Cache]] instance
     */
   class DefaultYaspProcessor(processor: Processor[Process], registry: Registry, cache: Cache)
       extends YaspProcessor {
