513987eea16efb5ee5e4852a3118ac2045a4f1c3
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-03-31 10:16:16 +0200

    Added YaspSource and YaspProcess partition number

---
+++ yasp-core/src/main/scala/it/yasp/core/spark/model/Session.scala
@@ -0,0 +1,16 @@
+package it.yasp.core.spark.model
+
+/** Session Product Type
+  *
+  * @param kind
+  *   a [[SessionType]]
+  * @param name
+  *   : Application name
+  * @param conf:
+  *   A [[Map]] of spark specific session configuration
+  */
+case class Session(
+    kind: SessionType,
+    name: String,
+    conf: Map[String, String]
+)
