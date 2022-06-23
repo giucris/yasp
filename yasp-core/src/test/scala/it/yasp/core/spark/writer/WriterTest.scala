edce98acd6e494882a323740ce7e9e011584ada1
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-06-24 01:32:58 +0200

    WIP

--- yasp-core/src/test/scala/it/yasp/core/spark/writer/WriterTest.scala
+++ yasp-core/src/test/scala/it/yasp/core/spark/writer/WriterTest.scala
@@ -1,18 +1,37 @@
 package it.yasp.core.spark.writer
 
-import it.yasp.core.spark.model.Dest.Parquet
-import it.yasp.core.spark.writer.Writer.ParquetWriter
+import it.yasp.core.spark.model.Dest.{Csv, Jdbc, Json, Parquet}
+import it.yasp.core.spark.writer.Writer.DestWriter
 import it.yasp.testkit.{SparkTestSuite, TestUtils}
-import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.types.DataTypes.StringType
 import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.{Dataset, Row}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
-class WriterTest extends AnyFunSuite with SparkTestSuite with BeforeAndAfterAll {
+import java.sql.Connection
+import java.sql.DriverManager.{getConnection, registerDriver}
+import java.util.Properties
 
+class WriterTest extends AnyFunSuite with SparkTestSuite with BeforeAndAfterAll {
+  registerDriver(new org.h2.Driver)
+
+  val writer = new DestWriter()
   private val workspace = "yasp-core/src/test/resources/WriterTest"
+  private val connUrl1: String = "jdbc:h2:mem:dbx"
+
+  private val df: Dataset[Row] = spark.createDataset(Seq(Row("a", "b", "c")))(
+    RowEncoder(
+      StructType(
+        Seq(
+          StructField("h0", StringType, nullable = true),
+          StructField("h1", StringType, nullable = true),
+          StructField("h2", StringType, nullable = true)
+        )
+      )
+    )
+  )
 
   override protected def beforeAll(): Unit = {
     TestUtils.cleanFolder(workspace)
@@ -25,60 +44,24 @@ class WriterTest extends AnyFunSuite with SparkTestSuite with BeforeAndAfterAll 
   }
 
   test("write parquet") {
-    new ParquetWriter().write(
-      spark.createDataset(Seq(Row("a", "b", "c")))(
-        RowEncoder(
-          StructType(
-            Seq(
-              StructField("h0", StringType, nullable = true),
-              StructField("h1", StringType, nullable = true),
-              StructField("h2", StringType, nullable = true)
-            )
-          )
-        )
-      ),
-      Parquet(s"$workspace/parquet1/")
-    )
-
-    val expected = spark.createDataset(Seq(Row("a", "b", "c")))(
-      RowEncoder(
-        StructType(
-          Seq(
-            StructField("h0", StringType, nullable = true),
-            StructField("h1", StringType, nullable = true),
-            StructField("h2", StringType, nullable = true)
-          )
-        )
-      )
-    )
-    val actual   = spark.read.parquet(s"$workspace/parquet1/")
-
-    assertDatasetEquals(actual, expected)
+    writer.write(df, Parquet(s"$workspace/parquet1/"))
+    assertDatasetEquals(spark.read.parquet(s"$workspace/parquet1/"), df)
   }
 
-  test("write parquet with partitionBy") {
-    new ParquetWriter().write(
-      spark.createDataset(Seq(Row("a", "b", "c")))(
-        RowEncoder(
-          StructType(
-            Seq(
-              StructField("h0", StringType, nullable = true),
-              StructField("h1", StringType, nullable = true),
-              StructField("h2", StringType, nullable = true)
-            )
-          )
-        )
-      ),
-      Parquet(s"$workspace/parquet2/", partitionBy = Seq("h0", "h1"))
-    )
-
-    val expected = spark.createDataset(Seq(Row("c")))(
-      RowEncoder(StructType(Seq(StructField("h2", StringType, nullable = true))))
-    )
-    val actual   = spark.read.parquet(s"$workspace/parquet2/h0=a/h1=b/")
-
-    assertDatasetEquals(actual, expected)
+  test("write csv") {
+    writer.write(df, Csv(s"$workspace/csv1/", Map("header" -> "true")))
+    assertDatasetEquals(spark.read.csv(s"$workspace/csv1/"), df)
   }
 
-  test("write json") {}
+  test("write json") {
+    writer.write(df, Json(s"$workspace/json1/"))
+    assertDatasetEquals(spark.read.csv(s"$workspace/json1/"), df)
+  }
+
+  test("write jdbc"){
+    writer.write(df,Jdbc(connUrl1,options=Map("dbTable"->"my_table")))
+    val actual = spark.read.jdbc(connUrl1,"my_table",new Properties())
+    assertDatasetEquals(actual, df)
+  }
+
 }
