346768559316d5b06069584a1cec6fe2bd247dc4
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-09-30 23:52:16 +0200

    Fix

--- yasp-core/src/test/scala/it/yasp/core/spark/writer/WriterTest.scala
+++ yasp-core/src/test/scala/it/yasp/core/spark/writer/WriterTest.scala
@@ -17,10 +17,10 @@ import java.util.Properties
 class WriterTest extends AnyFunSuite with SparkTestSuite with BeforeAndAfterAll {
   registerDriver(new org.h2.Driver)
 
-  val writer                   = new DestWriter()
-  private val workspace        = "yasp-core/src/test/resources/WriterTest"
-  private val connUrl1: String = "jdbc:h2:mem:dbx"
-  val connection: Connection   = getConnection(connUrl1)
+  val writer                         = new DestWriter()
+  private val workspace              = "yasp-core/src/test/resources/WriterTest"
+  private val connUrl1: String       = "jdbc:h2:mem:dbx"
+  private val connection: Connection = getConnection(connUrl1)
 
   private val df: Dataset[Row] = spark.createDataset(Seq(Row("a", "b", "c")))(
     RowEncoder(
