da124f2c455fa182e480c62d02b8181a33a6e29d
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-10-04 19:20:28 +0200

    Fix

--- project/Settings.scala
+++ project/Settings.scala
@@ -5,23 +5,23 @@ import wartremover.WartRemover.autoImport.{wartremoverErrors, Wart, Warts}
 object Settings {
 
   lazy val scalaCompilerSettings = Seq(
-   "-deprecation",
-   "-feature",
-   "-unchecked",
-   "-Xlint:_,-missing-interpolator",
-   "-encoding",
-   "UTF-8",
-   "-Xfatal-warnings",
-   "-Ypartial-unification",
-   "-Ywarn-dead-code",
-   "-Ywarn-inaccessible",
-   "-Ywarn-unused-import",
-   "-Ywarn-infer-any",
-   "-target:jvm-1.8",
-   "-language:implicitConversions",
-   "-language:higherKinds",
-   "-language:existentials",
-   "-language:postfixOps"
+    "-deprecation",
+    "-feature",
+    "-unchecked",
+    "-Xlint:_,-missing-interpolator",
+    "-encoding",
+    "UTF-8",
+    "-Xfatal-warnings",
+    "-Ypartial-unification",
+    "-Ywarn-dead-code",
+    "-Ywarn-inaccessible",
+    "-Ywarn-unused-import",
+    "-Ywarn-infer-any",
+    "-target:jvm-1.8",
+    "-language:implicitConversions",
+    "-language:higherKinds",
+    "-language:existentials",
+    "-language:postfixOps"
   )
 
   lazy val wartRemoverSettings = Seq(
