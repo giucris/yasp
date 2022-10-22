f1d87a707c7f3c02d8eee491dde9f4ff6626cae4
Giuseppe Cristiano <giucristiano89@gmail.com>
2022-10-22 17:02:00 +0200

    Added CustomWriter

--- yasp-core/src/test/scala/it/yasp/core/spark/plugin/MyTestWriterPlugin.scala
+++ yasp-core/src/test/scala/it/yasp/core/spark/plugin/MyTestWriterPlugin.scala
@@ -3,10 +3,9 @@ import org.apache.spark.sql.{Dataset, Row}
 
 class MyTestWriterPlugin extends WriterPlugin {
 
-  override def write(dataset: Dataset[Row], options: Option[Map[String, String]]): Unit = {
+  override def write(dataset: Dataset[Row], options: Option[Map[String, String]]): Unit =
     options.flatMap(_.get("path")) match {
       case Some(value) => dataset.write.parquet(value)
-      case None => ()
+      case None        => ()
     }
-  }
 }
