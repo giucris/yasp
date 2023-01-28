d5ced5f93e9c5729fd292363284bfbaf37f13c42
giucris <giucristiano89@gmail.com>
2023-01-28 21:25:31 +0100

    Added seq of YaspAction as YaspPlan

--- yasp-service/src/main/scala/it/yasp/service/model/YaspAction.scala
+++ yasp-service/src/main/scala/it/yasp/service/model/YaspAction.scala
@@ -6,6 +6,16 @@ sealed trait YaspAction extends Product with Serializable
 
 object YaspAction {
 
+  /** A YaspSource model
+    * @param id:
+    *   The unique ID of the source
+    * @param source:
+    *   An instance of [[Source]]
+    * @param partitions:
+    *   An Optional number of partition that will be used to reshuffle the dataset
+    * @param cache:
+    *   An Optional [[CacheLayer]]
+    */
   final case class YaspSource(
       id: String,
       dataset: String,
@@ -14,6 +24,16 @@ object YaspAction {
       cache: Option[CacheLayer]
   ) extends YaspAction
 
+  /** A YaspProcess model
+    * @param id:
+    *   The unique ID of the process result
+    * @param process:
+    *   An instance of [[Process]]
+    * @param partitions:
+    *   An Optional number of partition that will be used to reshuffle the dataset
+    * @param cache:
+    *   An Optional [[CacheLayer]]
+    */
   final case class YaspProcess(
       id: String,
       dataset: String,
@@ -22,6 +42,12 @@ object YaspAction {
       cache: Option[CacheLayer]
   ) extends YaspAction
 
+  /** YaspSink model
+    * @param id:
+    *   a [[String]] id of the data to store on the specific Dest
+    * @param dest:
+    *   a [[Dest]] instance
+    */
   final case class YaspSink(
       id: String,
       dataset: String,
@@ -43,66 +69,3 @@ object YaspAction {
     }
   }
 }
-
-//
-//package it.yasp.service.model
-//
-//import it.yasp.core.spark.model.{CacheLayer, DataOperations, Process}
-//
-///** A YaspProcess model
-//  * @param id:
-//  *   The unique ID of the process result
-//  * @param process:
-//  *   An instance of [[Process]]
-//  * @param partitions:
-//  *   An Optional number of partition that will be used to reshuffle the dataset
-//  * @param cache:
-//  *   An Optional [[CacheLayer]]
-//  */
-//final case class YaspProcess(
-//                              id: String,
-//                              dataset: String,
-//                              process: Process,
-//                              partitions: Option[Int] = None,
-//                              cache: Option[CacheLayer] = None
-//                            )
-//
-
-//
-//}
-
-//
-//package it.yasp.service.model
-//
-//import it.yasp.core.spark.model.{CacheLayer, DataOperations, Source}
-//
-///** A YaspSource model
-//  * @param id:
-//  *   The unique ID of the source
-//  * @param source:
-//  *   An instance of [[Source]]
-//  * @param partitions:
-//  *   An Optional number of partition that will be used to reshuffle the dataset
-//  * @param cache:
-//  *   An Optional [[CacheLayer]]
-//  */
-//final case class YaspSource(
-//                             id: String,
-//                             dataset: String,
-//                             source: Source,
-//                             partitions: Option[Int] = None,
-//                             cache: Option[CacheLayer] = None
-//                           )
-//
-//object YaspSource {
-//
-//  implicit class YaspSourceOps(yaspSource: YaspSource) {
-//    def dataOps: Option[DataOperations] =
-//      Some(DataOperations(yaspSource.partitions, yaspSource.cache))
-//        .flatMap {
-//          case DataOperations(None, None) => None
-//          case dataOps: DataOperations    => Some(dataOps)
-//        }
-//  }
-//
-//}
